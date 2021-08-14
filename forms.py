374d40ead0a391b22c9490d2052fcd755df0267b
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-08-14 18:16:02 -0400

    Step 5: Add Validation

--- forms.py
+++ forms.py
@@ -1,15 +1,18 @@
 """Form for adding a pet!  """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, IntegerField
+from wtforms import StringField, FloatField, IntegerField, SelectField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange
+
+species_choices = ["cat", "dog", "porcupine"]
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
     name = StringField("Pet name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
-    age = IntegerField("Age")
+    species = SelectField("Species", choices=[(s,s) for s in species_choices])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = IntegerField("Age", validators=[NumberRange(0,30)])
     notes = StringField("Notes")
 
 
