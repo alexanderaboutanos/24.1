8d8d657e33b698f54fd35ecfe7ac124d212a5b64
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-08-14 16:19:55 -0400

    Step 1: Create Database & Model

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, flash, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///agency"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///agency"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 debug = DebugToolbarExtension(app)
 
-# connect_db(app)
-
+connect_db(app)
 
 @app.route('/')
 def show_pet_list():
--- seed.py
+++ seed.py
@@ -1,2 +1,17 @@
+from app import db
+from models import Pet
+
 db.drop_all()
 db.create_all()
+
+test_pet = Pet(name="Stacy", species="serpent")
+
+db.session.add(test_pet)
+db.session.commit()
+
+pet1 = Pet(name="Tommy", species="cat", photo_url="https://urbananimalveterinary.com/wp-content/uploads/2019/10/small-kitten-1.jpg", age=5, notes="Spectacular Cat. He really knows how to have fun!", avaliable=False)
+pet2 = Pet(name="Steve", species="Porcupine", photo_url="https://pngimg.com/uploads/hedgehog/hedgehog_PNG6.png", age=1, notes="New Porcupine! So cute!", avaliable=True)
+pet3 = Pet(name="Leo", species="Leopard", photo_url="https://e7.pngegg.com/pngimages/607/542/png-clipart-leopard-animal-leopard.png", age=7, notes="He once bit his owner's arm off. Not as cute as you would think...", avaliable=True)
+
+db.session.add_all([pet1, pet2, pet3])
+db.session.commit()
