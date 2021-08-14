481de264dfb6a696ee97567eab9d8696a5ba8238
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-08-14 15:17:59 -0400

    Creating Initial Files 2

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, flash, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///agency"
+
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
