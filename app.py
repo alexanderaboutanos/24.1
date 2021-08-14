b6ac23a1152751dfbd4ace1976699d807cd10ec3
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-08-14 17:39:41 -0400

    Step 4: Create Handler for Add Pet Form

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///agency"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -17,10 +18,21 @@ def show_pet_list():
     pets = Pet.query.all()
     return render_template('pet_list.html', pets=pets)
 
-@app.route('/add')
-def show_add_pet_form():
-    
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """Renders new pet form (GET) or handles new pet form submission (POST)"""
     form = AddPetForm()
-    
-    return render_template('add_pet_form.html', form=form)
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes, avaliable=True)
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('add_pet_form.html', form=form)
 
